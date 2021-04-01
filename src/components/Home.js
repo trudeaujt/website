accf76a339f4c8ad91a447f1aa0a246eb523a8f2
Jonathan Trudeau <trudeaujt@gmail.com>
2021-04-01 17:01:37 +0900

    Built homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,16 @@
 import React from "react"
+import image from "../bg.jpeg";
 
+//@TODO make it so it doesn't scroll down
 export default function Home() {
-    return <h1>Home page!</h1>
+    return (
+        <main>
+            <img src={[image]} alt={"Kusatsu Onsen River"}
+                 className={"absolute object-cover w-full h-full"}/>
+            <section className={"relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8"}>
+                <h1 className={"text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name"}>
+                    Hello, I'm Jonathan</h1>
+            </section>
+        </main>
+    )
 }
