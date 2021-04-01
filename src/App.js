01317b9d5b72a03c4f4820133d53f6a11d6f6ae4
Jonathan Trudeau <trudeaujt@gmail.com>
2021-04-01 16:37:07 +0900

    Build out NavBar.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import About                          from "./components/About"
 import SinglePost                     from "./components/SinglePost"
 import Post                           from "./components/Post"
 import Project                        from "./components/Project"
+import NavBar                         from "./components/NavBar"
 
 function App() {
     return (
         <BrowserRouter>
+            <NavBar/>
             <Switch>
                 <Route component={Home} path='/'
                        exact/> {/*exact for home so it won't get confused with the other pages*/}
